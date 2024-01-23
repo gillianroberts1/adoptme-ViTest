52257a8b5a98c7765c77079d45bc83eb50b63898
gillianroberts1 <138917227+gillianroberts1@users.noreply.github.com>
2024-01-23 16:06:54 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { createRoot } from "react-dom/client";
+import { useState } from "react";
+import SearchParams from "./SearchParams";
+import { StrictMode } from "react";
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Details from "./Details";
+import AdoptedPetContext from "./AdoptedPetContext";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
+
+const App = () => {
+  const adoptedPet = useState(null)
+  return (
+    <StrictMode>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <AdoptedPetContext.Provider value={adoptedPet}>
+          <header>
+            <Link to="/"> Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+          </AdoptedPetContext.Provider>
+        </QueryClientProvider>
+      </BrowserRouter>
+    </StrictMode>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
+
+// export default App;
